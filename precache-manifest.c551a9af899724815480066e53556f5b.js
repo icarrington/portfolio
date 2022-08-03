2c1d7070b61df0177ad7809b41eefaf9e005ab85
icarrington <isaiah.carrington@uconn.edu>
2022-08-02 23:37:32 -0400

    Updates

---
+++ precache-manifest.c551a9af899724815480066e53556f5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "58055a6caf623b1ef941",
+    "url": "/portfolio/static/css/main.af78397c.chunk.css"
+  },
+  {
+    "revision": "58055a6caf623b1ef941",
+    "url": "/portfolio/static/js/main.58055a6c.chunk.js"
+  },
+  {
+    "revision": "9a5ad68e4c7b9337c49c",
+    "url": "/portfolio/static/js/1.9a5ad68e.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a9d365fba590af316673201c7bd6808a",
+    "url": "/portfolio/static/media/home_anime.a9d365fb.gif"
+  },
+  {
+    "revision": "5417dce2e4e4b5b19be6bf14f5797130",
+    "url": "/portfolio/static/media/about_vector.5417dce2.png"
+  },
+  {
+    "revision": "5e561a9d0c627b9a734d64e4d0ad4d85",
+    "url": "/portfolio/static/media/IMG_4939.5e561a9d.JPG"
+  },
+  {
+    "revision": "6ccace210b47ca05db83c7d7aa36fcee",
+    "url": "/portfolio/static/media/skills_vector.6ccace21.png"
+  },
+  {
+    "revision": "2b0bea9499cc77f0549826f3580806e6",
+    "url": "/portfolio/static/media/contact_anime.2b0bea94.png"
+  },
+  {
+    "revision": "3d74e8bfd4ef7985f7529bb9f7650eca",
+    "url": "/portfolio/static/media/twitter.3d74e8bf.png"
+  },
+  {
+    "revision": "8b1d4bba2b7fd0ac7ec8ba2117df089d",
+    "url": "/portfolio/static/media/email.8b1d4bba.png"
+  },
+  {
+    "revision": "21dfecc6ad742b8cd73b99b85987bf67",
+    "url": "/portfolio/index.html"
+  }
+];
